b7037f2d716aac1f147421e4bed25894246fe02c
jolyne-mangeot <joseph.mangeot@laplateforme.io>
2025-06-05 14:36:41 +0200

    progress: add unity assets folders and base compilation for dll integration

---
+++ Tower-Defense/Assets/Scripts/PluginTest.cs
@@ -0,0 +1,14 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PluginTest : MonoBehaviour
+{
+    [DllImport("libTest")]
+    private static extern int Multiply(int a, int b);
+
+    void Start()
+    {
+        int result = Multiply(2, 3);
+        Debug.Log("2 * 3 = " + result); // Devrait afficher : "2 * 3 = 5"
+    }
+}
