96476d4eb2bfd1a622458675a47b4a26575ff024
joseph-mangeot <joseph.mangeot@laplateforme.io>
2025-06-10 22:20:04 +0200

    progress: add level_core class with json parsing and data recovery

---
+++ game/Level_core/Level_core.cpp
@@ -0,0 +1,48 @@
+//
+// Created by josur on 10/06/2025.
+//
+
+#include "Level_core.hpp"
+
+Level_core::Level_core(const string &level_json_path) {
+
+    ifstream level_json_file(level_json_path);
+
+    basic_json<> level_data = json::parse(level_json_file);
+
+    this->number_of_checkpoints = level_data.at("number_of_checkpoints");
+    this->number_of_turrets = level_data.at("number_of_turrets");
+    this->number_of_enemies = level_data.at("number_of_enemies");
+    this->number_of_waves = level_data.at("number_of_waves");
+
+    this->checkpoints_coordinates.reserve(number_of_checkpoints);
+    this->turrets_coordinates.reserve(number_of_turrets);
+    this->waves.reserve(number_of_waves);
+
+    for (int i = 0; i < number_of_checkpoints; i++) {
+        basic_json<> checkpoint_coordinates = level_data.at("checkpoints_coordinates").at(to_string(i));
+        array<int, 2> coordinates = {checkpoint_coordinates.at("x"), checkpoint_coordinates.at("z")};
+        this->checkpoints_coordinates.push_back(coordinates);
+    }
+
+    for (int i = 0; i < number_of_turrets; i++) {
+        basic_json<> turret_coordinates = level_data.at("turrets_coordinates").at(to_string(i));
+        array<int, 2> coordinates = {turret_coordinates.at("x"), turret_coordinates.at("z")};
+        this->turrets_coordinates.push_back(coordinates);
+    }
+
+    for (int i = 0; i < number_of_waves; i++) {
+        basic_json<> wave_data = level_data.at("wave_" + to_string(i + 1));
+
+        level_waves wave;
+
+        wave.number_of_enemies = wave_data.at("spawn_amount");
+        wave.spawn_interval = wave_data.at("spawn_interval");
+        wave.spawn_order.reserve(wave.number_of_enemies);
+
+        for (int j = 0; j < wave.number_of_enemies; j++) {
+            wave.spawn_order.push_back(wave_data.at("spawn_order").at(to_string(j + 1)));
+        }
+        this->waves.push_back(wave);
+    }
+}
