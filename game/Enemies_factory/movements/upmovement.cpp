8117cabada860b0b4267412924b52552c5cc2b0a
Adeline <adeline.patenne@hotmail.fr>
2025-06-12 09:51:09 +0200

    fix: movement to get to checkpoint exactly functional

--- game/Enemies_factory/movements/upmovement.cpp
+++ game/Enemies_factory/movements/upmovement.cpp
@@ -10,7 +10,9 @@ using std::endl;
 
 void UpMovement::move(Enemy& enemy, const std::array<int, 2>& checkpoint) {
     if (enemy.getY() + enemy.getSpeed() > checkpoint[1]) {
-        enemy.setSpeed(abs(enemy.getY()+enemy.getSpeed()-checkpoint[1]));
+        // enemy.setSpeed(abs(enemy.getY()+enemy.getSpeed()-checkpoint[1]));
+        enemy.setSpeed(checkpoint[1] - enemy.getY());
+        // enemy.setSpeed(checkpoint[1]);
     }
     enemy.setY(static_cast<int>(enemy.getSpeed()));
     cout<<"↑ : L'ennemi se déplace x : "<<enemy.getX()<<" y : "<<enemy.getY()<<endl;
